86549044c8c9ad8423855073205f892d7025aed6
mhcodner <mhcodner@gmail.com>
2015-09-06 16:57:28 +1000

    Added static page for social crawlers

--- functions.php
+++ functions.php
@@ -7,42 +7,45 @@
  * @package SamVennPhoto
  */
 
-if ( ! function_exists( 'samvennphoto_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function samvennphoto_setup() {
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
+if (!function_exists('samvennphoto_setup')) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function samvennphoto_setup()
+    {
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
 
-	remove_action('wp_head', 'wp_generator');
-	remove_action('wp_head', 'wlwmanifest_link');
-	remove_action('wp_head', 'rsd_link');
-	remove_action('wp_head', 'wp_shortlink_wp_head');
-	remove_action('set_comment_cookies', 'wp_set_comment_cookies');
+        remove_action('wp_head', 'wp_generator');
+        remove_action('wp_head', 'wlwmanifest_link');
+        remove_action('wp_head', 'rsd_link');
+        remove_action('wp_head', 'wp_shortlink_wp_head');
+        remove_action('set_comment_cookies', 'wp_set_comment_cookies');
 
-	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
+        remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
 
-	add_filter('the_generator', '__return_false');
-	add_filter('show_admin_bar','__return_false');
-	add_filter('embed_oembed_html', 'oembed_html', 9999, 4);
+        add_filter('the_generator', '__return_false');
+        add_filter('show_admin_bar', '__return_false');
+        add_filter('embed_oembed_html', 'oembed_html', 9999, 4);
 
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-}
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('wp_print_styles', 'print_emoji_styles');
+    }
 endif; // samvennphoto_setup
-function oembed_html($html, $url, $attr, $post_id) {
-	return '<div class="video-wrapper">' . $html . '</div>';
+function oembed_html($html, $url, $attr, $post_id)
+{
+    return '<div class="video-wrapper">' . $html . '</div>';
 }
-add_action( 'after_setup_theme', 'samvennphoto_setup' );
+
+add_action('after_setup_theme', 'samvennphoto_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -51,24 +54,47 @@ add_action( 'after_setup_theme', 'samvennphoto_setup' );
  *
  * @global int $content_width
  */
-function samvennphoto_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'samvennphoto_content_width', 640 );
+function samvennphoto_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('samvennphoto_content_width', 640);
 }
-add_action( 'after_setup_theme', 'samvennphoto_content_width', 0 );
+
+add_action('after_setup_theme', 'samvennphoto_content_width', 0);
 
 /**
  * Enqueue scripts and styles.
  */
-function samvennphoto_scripts() {
-	wp_enqueue_style( 'samvennphoto-style', get_stylesheet_uri() );
+function samvennphoto_scripts()
+{
+    wp_enqueue_style('samvennphoto-style', get_stylesheet_uri());
 
-	wp_enqueue_script( 'samvennphoto-main', get_template_directory_uri() . '/js/bundle.js', array(), '', true );
+    wp_enqueue_script('samvennphoto-main', get_template_directory_uri() . '/js/bundle.js', array(), '', true);
 
-	// Variables for app script
-	wp_localize_script( 'samvennphoto-js-vars', 'samvennphotoJS',
-		array(
-			'themeuri' => get_template_directory_uri(),
-		)
-	);
+    // Variables for app script
+    wp_localize_script('samvennphoto-js', 'samvennphotoJS',
+        array(
+            'themeuri' => get_template_directory_uri(),
+        )
+    );
 }
-add_action( 'wp_enqueue_scripts', 'samvennphoto_scripts' );
+
+add_action('wp_enqueue_scripts', 'samvennphoto_scripts');
+
+/**
+ * Inserts an array of strings into a file (.htaccess ), placing it between
+ * BEGIN and END markers. Replaces existing marked info. Retains surrounding
+ * data. Creates file if none exists.
+ *
+ * @param array|string $insertion
+ * @return bool True on write success, false on failure.
+ */
+function add_htaccess($insertion)
+{
+    $htaccess_file = ABSPATH . '.htaccess';
+    return insert_with_markers($htaccess_file, 'SVP', (array)$insertion);
+}
+
+$htaccess_rules = [
+    'RewriteCond %{HTTP_USER_AGENT} (facebookexternalhit/[0-9]|Twitterbot|Pinterest|Google.*snippet|Googlebot|redditbot|bingbot)',
+    'RewriteRule blog/(\d*)/.*$ %%SITE_URL%%static-page.php?id=$1 [P]'
+];
---
+++ static-page.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file creates a static page for crawlers such as Facebook or Twitter bots that cannot evaluate JavaScript.
+ *
+ * For a full explanation see https://github.com/michaelbromley/angular-social-demo
+ */
+
+$API_URL = 'http://' . $_SERVER['SERVER_NAME'] . '/' . $_GET['slug'] . '?json=1';
+
+$jsonData = getData($API_URL);
+makePage($jsonData);
+
+function getData($API_URL)
+{
+    $rawData = file_get_contents($API_URL);
+    return json_decode($rawData);
+}
+
+function makePage($data)
+{
+    global $API_URL;
+
+    if (chop($API_URL, "?json=1") === 'http://' . $_SERVER['SERVER_NAME'] . '/') {
+        $pageUrl = 'http://' . $_SERVER['SERVER_NAME'] . '/';
+        $pageTitle = 'Sam Venn Photography';
+        $metaDescription = 'Photographer from Sydney';
+        $pageContent = '';
+    }
+    elseif (isset($data->post)) {
+        $pageUrl = $data->post->url;
+        $pageTitle = $data->post->title;
+        $metaDescription = substr(strip_tags($data->post->excerpt), 0, 155);
+        $pageContent = $data->post->content;
+    }
+    else {
+        $pageUrl = $data->page->url;
+        $pageTitle = $data->page->title;
+        $metaDescription = substr(strip_tags($data->page->excerpt), 0, 155);
+        $pageContent = $data->page->content;
+    }
+    $thumbUrl = isset($data->post->thumbnail) ? $data->post->thumbnail : 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content/themes/SVPWebsite/img/SVP_Logo.png';
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title><?php echo $pageTitle; ?></title>
+        <meta property="description" content="<?php echo $metaDescription; ?>"/>
+
+        <!-- Twitter summary card metadata -->
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:title" content="<?php echo $pageTitle; ?>"/>
+        <meta property="twitter:description" content="<?php echo $metaDescription ?>"/>
+        <meta property="twitter:url" content="<?php echo $pageUrl; ?>"/>
+        <meta property="twitter:image" content="<?php echo $thumbUrl; ?>"/>
+
+        <!-- Facebook, Pinterest, Google Plus and others make use of open graph metadata -->
+        <meta property="og:title" content="<?php echo $pageTitle; ?>"/>
+        <meta property="og:description" content="<?php echo $metaDescription; ?>"/>
+        <meta property="og:type" content="article"/>
+        <meta property="og:url" content="<?php echo $pageUrl; ?>"/>
+            <meta property="og:image" content="<?php echo $thumbUrl; ?>"/>
+
+    </head>
+    <body>
+        <h1 class="page-header"><?php echo $pageTitle; ?></h1>
+
+        <div class="post-body">
+            <?php echo $pageContent; ?>
+        </div>
+    </body>
+    </html>
+    <?php
+}
