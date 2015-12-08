fd27b8672f2bd7eac04de0c5bfaec3700ec7dab7
mhcodner <mhcodner@gmail.com>
2015-12-08 19:25:18 +1100

    Change font

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Cousine:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
     <?php wp_head(); ?>
 </head>
 <body ng-cloak>
