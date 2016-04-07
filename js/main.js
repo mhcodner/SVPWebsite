940382a8bb1a7426becf6d50445049f08397d9be
Michael Codner <mhcodner@gmail.com>
2016-04-07 14:02:02 +1000

    fix wrong images showing up

--- js/main.js
+++ js/main.js
@@ -15,29 +15,20 @@ function initMaterialBox() {
 
 var gallery;
 
-function initLightBox(event) {
-    event.preventDefault();
-    event = event || window.event;
-    var target = event.target || event.srcElement;
-    var link = target.src ? target.parentNode : target;
-    var options = {index: link, urlProperty: 'href'};
-    var links = event.target.parentNode.parentNode.getElementsByTagName('a');
-    gallery = blueimp.Gallery(links, options);
-}
-
 function initialiseSlider() {
     var path = window.location.pathname;
     var container = '#blueimp-gallery-carousel';
+    var category = "";
     if (path === '/') {
         container = '#blueimp-gallery-carousel-home';
     }
     else if (path !== '/gallery/') {
-        var category = path.split('/')[3];
+        category = path.split('/')[3];
         console.log(category);
         container = '#blueimp-gallery-carousel' + category;
     }
     gallery = blueimp.Gallery(
-        document.getElementById('links').getElementsByTagName('a'),
+        document.getElementById('links' + category).getElementsByTagName('a'),
         {
             container: container,
             carousel: true,
